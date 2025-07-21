052bde92c53f747be0b58c6a4e147808b1972bc7
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-07-21 13:03:54 -0600

    refactor(task): introduce the task handler to handle the worker tasks as it was a http server

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -3,19 +3,82 @@ package task
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"fmt"
+	"log/slog"
+	"time"
 
 	"github.com/hibiken/asynq"
+	"github.com/jmoiron/sqlx"
+
+	"github.com/danvergara/nostrich_watch_monitor/pkg/healthcheck"
 )
 
 const (
 	TypeHealthCheck = "relay:healthcheck"
 )
 
+type TasKHandler struct {
+	db         *sqlx.DB
+	timeout    time.Duration
+	privateKey string // For signing test events
+	logger     *slog.Logger
+	redisHost  string
+}
+
+func NewTaskHandler(
+	db *sqlx.DB,
+	timeout time.Duration,
+	privateKey string,
+	logger *slog.Logger,
+	redisHost string,
+) *TasKHandler {
+	return &TasKHandler{
+		db:         db,
+		timeout:    timeout,
+		privateKey: privateKey,
+		logger:     logger,
+		redisHost:  redisHost,
+	}
+}
+
+func (th *TasKHandler) Run() error {
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: th.redisHost},
+		asynq.Config{Concurrency: 10},
+	)
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(TypeHealthCheck, th.HandleRelayHealthCheckTask)
+
+	if err := srv.Run(mux); err != nil {
+		th.logger.Error(err.Error())
+		return err
+	}
+
+	return nil
+}
+
 // Payload for any task related to health checks on Nostr relays.
 type RelayHealthCheckTaskPayload struct {
 	// URL of the relay
 	RelayURL string
+}
+
+func (th *TasKHandler) HandleRelayHealthCheckTask(ctx context.Context, t *asynq.Task) error {
+	var r RelayHealthCheckTaskPayload
+
+	if err := json.Unmarshal(t.Payload(), &r); err != nil {
+		return err
+	}
+
+	rc := healthcheck.NewRelayChecker(th.db, th.timeout, th.privateKey, th.logger)
+	if err := rc.CheckRelay(ctx, r.RelayURL); err != nil {
+		return err
+	}
+
+	th.logger.Info(fmt.Sprintf("[*] health checking %s Nostr relay", r.RelayURL))
+
+	return nil
 }
 
 func NewRelayHealthCheckTask(relayURL string) (*asynq.Task, error) {
@@ -25,13 +88,4 @@ func NewRelayHealthCheckTask(relayURL string) (*asynq.Task, error) {
 	}
 
 	return asynq.NewTask(TypeHealthCheck, payload), nil
-}
-
-func HandleRelayHealthCheckTask(ctx context.Context, t *asynq.Task) error {
-	var r RelayHealthCheckTaskPayload
-	if err := json.Unmarshal(t.Payload(), &r); err != nil {
-		return err
-	}
-	log.Printf("[*] Health checking %s Nostr relay", r.RelayURL)
-	return nil
 }
