422546307dab86cbca7cdf242db4e213545f4dfb
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-07-21 13:01:33 -0600

    refactor(repository): add methods to update the relay and the health_checks tables

--- pkg/repository/postgres/relay_repository.go
+++ pkg/repository/postgres/relay_repository.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 
@@ -28,4 +29,48 @@ func (r *relayRepository) List(ctx context.Context) ([]domain.Relay, error) {
 	}
 
 	return relays, nil
+}
+
+func (r *relayRepository) Update(ctx context.Context, relayInfo domain.Relay) error {
+	query := `
+        UPDATE relays SET 
+            name = :name,
+            description = :description,
+            pubkey = :pubkey,
+            contact = :contact,
+            supported_nips = :supported_nips,
+            software = :software,
+            version = :version,
+            icon = :icon,
+            banner = :banner,
+            privacy_policy = :privacy_policy,
+            terms_of_service = :terms_of_service,
+            posting_policy = :posting_policy,
+            updated_at = CURRENT_TIMESTAMP
+        WHERE url = :url
+    `
+
+	if _, err := r.db.NamedExec(query, relayInfo); err != nil {
+		return fmt.Errorf("failed to update relay info: %w", err)
+	}
+
+	return nil
+}
+
+func (r *relayRepository) SaveHealthCheck(ctx context.Context, status domain.HealthCheck) error {
+	query := `
+        INSERT INTO health_checks (
+            relay_url, created_at, websocket_success, websocket_error,
+            nip11_success, nip11_error, rtt_open, rtt_read, rtt_write, rtt_nip11
+        ) VALUES (
+					:relay_url, :created_at, :websocket_success, :websocket_error,
+        	:nip11_success, :nip11_error, :rtt_open, :rtt_read, :rtt_write, :rtt_nip11
+				)
+    `
+	_, err := r.db.NamedExec(query, status)
+	if err != nil {
+		return fmt.Errorf("failed to save health check: %w", err)
+	}
+
+	return nil
 }
