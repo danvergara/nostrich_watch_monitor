7745d7e4d228c4b8e9026bc0891b00acb3883724
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-07-21 13:05:51 -0600

    refactor(worker): add the new code and changes to the worker to process health checks tasks

--- cmd/worker.go
+++ cmd/worker.go
@@ -6,12 +6,15 @@ package cmd
 import (
 	"log/slog"
 	"os"
+	"time"
 
-	"github.com/hibiken/asynq"
 	"github.com/spf13/cobra"
 
+	"github.com/danvergara/nostrich_watch_monitor/pkg/database"
 	"github.com/danvergara/nostrich_watch_monitor/pkg/task"
 )
+
+var monitorPrivateKey string
 
 // workerCmd represents the worker command
 var workerCmd = &cobra.Command{
@@ -25,15 +28,24 @@ var workerCmd = &cobra.Command{
 
 		logger := slog.New(slog.NewJSONHandler(os.Stdout, opts))
 
-		srv := asynq.NewServer(
-			asynq.RedisClientOpt{Addr: redisHost},
-			asynq.Config{Concurrency: 10},
-		)
+		dbConfig := database.Config{
+			Host:     dbHost,
+			Port:     dbPort,
+			User:     dbUser,
+			Password: dbPass,
+			DBName:   dbName,
+		}
 
-		mux := asynq.NewServeMux()
-		mux.HandleFunc(task.TypeHealthCheck, task.HandleRelayHealthCheckTask)
+		// Create a PostgreSQL database pool of connections given config data.
+		db, err := database.NewPostgresDB(dbConfig)
+		if err != nil {
+			return err
+		}
+		defer db.Close()
 
-		if err := srv.Run(mux); err != nil {
+		th := task.NewTaskHandler(db, 10*time.Second, monitorPrivateKey, logger, redisHost)
+
+		if err := th.Run(); err != nil {
 			logger.Error(err.Error())
 			return err
 		}
@@ -43,5 +55,6 @@ var workerCmd = &cobra.Command{
 }
 
 func init() {
+	monitorPrivateKey = os.Getenv("NOSTRICH_WATCH_MONITOR_PRIVATE_KEY")
 	rootCmd.AddCommand(workerCmd)
 }
