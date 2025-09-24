ade18143ffa884477e148c88705cc1ce4de2d454
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2025-09-24 15:11:10 -0600

    chore(task): add a metrics server to expose metrics to prometheus

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -5,10 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/hibiken/asynq"
 	"github.com/jmoiron/sqlx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sys/unix"
 
 	"github.com/danvergara/nostrich_watch_monitor/pkg/healthcheck"
 )
@@ -16,6 +23,33 @@ import (
 const (
 	TypeHealthCheck         = "relay:healthcheck"
 	TypeMonitorAnnouncement = "relay:announcement"
+)
+
+// Metric variables.
+var (
+	processedCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "processed_tasks_total",
+			Help: "Total number of processed tasks",
+		},
+		[]string{"task_type"},
+	)
+
+	failedCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "failed_tasks_total",
+			Help: "Total number of failed tasks processed",
+		},
+		[]string{"task_type"},
+	)
+
+	inProgressGauge = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "in_progress_tasks",
+			Help: "Current number of tasks being processed",
+		},
+		[]string{"task_type"},
+	)
 )
 
 type TasKHandler struct {
@@ -46,19 +80,53 @@ func NewTaskHandler(
 }
 
 func (th *TasKHandler) Run() error {
+	httpServeMux := http.NewServeMux()
+	httpServeMux.Handle("/metrics", promhttp.Handler())
+	metricsSrv := &http.Server{
+		Addr:    ":2112",
+		Handler: httpServeMux,
+	}
+	done := make(chan struct{})
+
+	// Start the metrics server.
+	go func() {
+		if err := metricsSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			th.logger.Error("metrics server errored", slog.Any("error", err.Error()))
+		}
+
+		close(done)
+	}()
+
 	srv := asynq.NewServer(
 		asynq.RedisClientOpt{Addr: th.redisHost},
 		asynq.Config{Concurrency: 10},
 	)
 
 	mux := asynq.NewServeMux()
+	mux.Use(metricsMiddleware)
 	mux.HandleFunc(TypeHealthCheck, th.HandleRelayHealthCheckTask)
 	mux.HandleFunc(TypeMonitorAnnouncement, th.HandleMonitorAnnouncementTask)
 
-	if err := srv.Run(mux); err != nil {
-		th.logger.Error(err.Error())
+	if err := srv.Start(mux); err != nil {
+		th.logger.Error("Failed to start worker server", slog.Any("error", err.Error()))
 		return err
 	}
+
+	// Wait for termination signals.
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, unix.SIGTERM, unix.SIGINT)
+
+	select {
+	case sig := <-sigs:
+		th.logger.Info("received signal", slog.Any("signal", sig))
+	case <-done:
+		th.logger.Info(
+			"metrics server shut down unexpectedly",
+		)
+	}
+
+	srv.Stop()
+	th.logger.Info("worker server stopped")
 
 	return nil
 }
@@ -91,7 +159,7 @@ func (th *TasKHandler) HandleRelayHealthCheckTask(ctx context.Context, t *asynq.
 		return err
 	}
 
-	th.logger.Info(fmt.Sprintf("[*] health checking %s Nostr relay", r.RelayURL))
+	th.logger.Info("[*] health checking", slog.String("nostr_relay", r.RelayURL))
 
 	return nil
 }
@@ -118,6 +186,19 @@ func (th *TasKHandler) HandleMonitorAnnouncementTask(ctx context.Context, t *asy
 	}
 
 	return nil
+}
+
+func metricsMiddleware(next asynq.Handler) asynq.Handler {
+	return asynq.HandlerFunc(func(ctx context.Context, t *asynq.Task) error {
+		inProgressGauge.WithLabelValues(t.Type()).Inc()
+		err := next.ProcessTask(ctx, t)
+		inProgressGauge.WithLabelValues(t.Type()).Dec()
+		if err != nil {
+			failedCounter.WithLabelValues(t.Type()).Inc()
+		}
+		processedCounter.WithLabelValues(t.Type()).Inc()
+		return err
+	})
 }
 
 func NewRelayHealthCheckTask(relayURL string) (*asynq.Task, error) {
